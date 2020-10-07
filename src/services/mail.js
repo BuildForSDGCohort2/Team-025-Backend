abfb22fa97a7b67ce6de45ce230141cdddccde04
Lutor Ayangaor <lutor4148@gmail.com>
2020-10-07 18:48:54 +0100

    password reset

--- src/services/mail.js
+++ src/services/mail.js
@@ -50,3 +50,37 @@ exports.sendVerificationMail = async (user) => {
 
   return info.messageId;
 };
+
+
+
+/* send email
+const subject = 'Password change request';
+const to = user.email;
+const from = process.env.FROM_EMAIL;
+const link = `http://${req.headers.host}/api/auth/reset/${user.resetPasswordToken}`;
+const html = `<p>Hi ${user.username}</p>
+              <p>Please click on the following <a href="${link}">link</a> to reset your password.</p> 
+              <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`;
+
+await sendEmail({
+  to, from, subject, html
+});
+
+res.status(200).json({ message: `A reset email has been sent to ${user.email}.` });
+} catch (error) {
+res.status(500).json({ message: error.message });
+}
+};
+
+
+const subject = 'Your password has been changed';
+    const to = user.email;
+    const from = process.env.FROM_EMAIL;
+    const html = `<p>Hi ${user.username}</p>
+                  <p>This is a confirmation that the password for your account ${user.email} has just been changed.</p>`;
+
+    await sendEmail({
+      to, from, subject, html
+    });
+*/
+
