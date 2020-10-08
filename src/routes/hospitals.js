40ca8ff3b4846b5b7e51dd21ce63b7858e338580
Kamil <kamilBalogun1@gmail.com>
2020-10-08 11:21:03 +0100

    Revert "Revert "feat(hospital): add hospital routes""

--- src/routes/hospitals.js
+++ src/routes/hospitals.js
@@ -1,11 +1,41 @@
 // const appointmentController = require('../controllers/appointments');
-const { all, one, byLg } = require('../controllers/hospitalController');
+const { all, one, byLg,
+  createHospital,
+  modifyHospitalDetail } = require('../controllers/hospitalController');
+const { acceptRequest,
+  rejectRequest,
+  completeRequest } = require('../controllers/requestController');
 const auth = require('../middleware/auth');
+const roleMiddleware = require("../middleware/roleMiddleware");
+const { UserRole } = require("../models/user");
 
 const hospitalRoutes = (router) => {
-  router.route('/hospitals/').get(auth.allowIfLoggedin, all);
+  router.route('/hospitals/')
+    .get(auth.allowIfLoggedin, all)
+    .post(auth.allowIfLoggedin, createHospital);
+
   router.route('/hospitals/:lg').get(auth.allowIfLoggedin, byLg);
-  router.route('/hospitals/hospital/:id').get(auth.allowIfLoggedin, one);
+
+  router.route('/hospitals/hospital/:id')
+    .get(auth.allowIfLoggedin, one)
+    .post(auth.allowIfLoggedin,
+      roleMiddleware({ allowedRoles: [UserRole.Admin, UserRole.Hospital] }),
+      modifyHospitalDetail);
+
+  router.route('/hospitals/hospital/acceptreq/:id')
+    .post(auth.allowIfLoggedin,
+      roleMiddleware({ allowedRoles: [UserRole.Admin, UserRole.Hospital] }),
+      acceptRequest)
+
+  router.route('/hospitals/hospital/rejectreq/:id')
+    .post(auth.allowIfLoggedin,
+      roleMiddleware({ allowedRoles: [UserRole.Admin, UserRole.Hospital] }),
+      rejectRequest)
+
+  router.route('/hospitals/hospital/completereq/:id')
+    .post(auth.allowIfLoggedin,
+      roleMiddleware({ allowedRoles: [UserRole.Admin, UserRole.Hospital] }),
+      completeRequest);
 };
 
 module.exports = hospitalRoutes;
